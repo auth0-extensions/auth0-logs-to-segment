926d73272cf3691ee59c2d68031714100d5388c2
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2017-06-02 16:00:27 +0300

    logLevel and logTypes added to webtask.json, processor updated

--- server/lib/processLogs.js
+++ server/lib/processLogs.js
@@ -1,4 +1,3 @@
-const _ = require('lodash');
 const async = require('async');
 const Segment = require('analytics-node');
 const tools = require('auth0-extension-tools');
@@ -17,77 +16,57 @@ module.exports = (storage) =>
       return next();
     }
 
-    return tools.managementApi.getClient({
+    const analytics = new Segment(config('SEGMENT_KEY'));
+
+    const onLogsReceived = (logs, callback) => {
+      if (!logs || !logs.length) {
+        return callback();
+      }
+
+      logger.info(`Sending ${logs.length} logs to Segment.`);
+
+      async.each(logs, (log, cb) => {
+        const event = {
+          event: loggingTools.logTypes.get(log.type),
+          properties: log
+        };
+
+        if (log.user_id) {
+          event.userId = log.user_id;
+        } else {
+          event.anonymousId = log._id;
+        }
+
+        analytics.track(event, cb);
+      }, callback);
+    };
+
+    const slack = new loggingTools.reporters.SlackReporter({
+      hook: config('SLACK_INCOMING_WEBHOOK_URL'),
+      username: 'auth0-logs-to-segment',
+      title: 'Logs To Segment'
+    });
+
+    const options = {
       domain: config('AUTH0_DOMAIN'),
       clientId: config('AUTH0_CLIENT_ID'),
       clientSecret: config('AUTH0_CLIENT_SECRET'),
-    })
-      .then(auth0 => {
-        const analytics = new Segment(config('SEGMENT_KEY'));
+      batchSize: config('BATCH_SIZE'),
+      startFrom: config('START_FROM'),
+      logTypes: config('LOG_TYPES'),
+      logLevel: config('LOG_LEVEL')
+    };
 
-        const onLogsReceived = (logs, callback) => {
-          if (!logs || !logs.length) {
-            return callback();
-          }
+    const auth0logger = new loggingTools.LogsProcessor(storage, options);
 
-          logger.info(`Sending ${logs.length} logs to Segment.`);
-
-          async.eachLimit(logs, 10, (log, cb) => {
-            if (!log.user_id) {
-              return cb();
-            }
-
-            return auth0.users.get({ id: log.user_id })
-              .then((user) => {
-                analytics.track({
-                  userId: log.user_id,
-                  event: loggingTools.logTypes.get(log.type),
-                  properties: _.extend({}, user.user_metadata, _.omit(user, ['user_metadata', 'app_metadata']), user.app_metadata)
-                }, cb);
-              })
-              .catch((err) => {
-                if(err.statusCode === 404) {
-                  return analytics.track({
-                    userId: log.user_id,
-                    event: loggingTools.logTypes.get(log.type),
-                    properties: {}
-                  }, cb);
-                }
-
-                return cb(err);
-              });
-          }, callback);
-        };
-
-        const slack = new loggingTools.reporters.SlackReporter({
-          hook: config('SLACK_INCOMING_WEBHOOK_URL'),
-          username: 'auth0-logs-to-segment',
-          title: 'Logs To Segment'
-        });
-
-        const options = {
-          domain: config('AUTH0_DOMAIN'),
-          clientId: config('AUTH0_CLIENT_ID'),
-          clientSecret: config('AUTH0_CLIENT_SECRET'),
-          batchSize: config('BATCH_SIZE') || 20,
-          startFrom: config('START_FROM'),
-          logTypes: [ 's', 'ss', 'f' ]
-        };
-
-        const auth0logger = new loggingTools.LogsProcessor(storage, options);
-
-        return auth0logger
-          .run(onLogsReceived)
-          .then(result => {
-            slack.send(result.status, result.checkpoint);
-            res.json(result);
-          })
-          .catch(err => {
-            slack.send({ error: err, logsProcessed: 0 }, null);
-            next(err);
-          });
+    return auth0logger
+      .run(onLogsReceived)
+      .then(result => {
+        slack.send(result.status, result.checkpoint);
+        res.json(result);
       })
-      .catch(function(err) {
+      .catch(err => {
+        slack.send({ error: err, logsProcessed: 0 }, null);
         next(err);
       });
   };
