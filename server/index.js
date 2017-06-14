d13673e649b15d13d31268cf219520f047c5d96f
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2017-06-14 11:01:37 +0300

    cleanup

--- server/index.js
+++ server/index.js
@@ -25,11 +25,6 @@ module.exports = (configProvider, storageProvider) => {
     stream: logger.stream
   }));
 
-  app.use(processLogs(storage));
-
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-
   // Configure routes.
   app.use(expressTools.routes.dashboardAdmins({
     secret: config('EXTENSION_SECRET'),
@@ -42,10 +37,13 @@ module.exports = (configProvider, storageProvider) => {
     sessionStorageKey: 'logs-to-segment:apiToken',
     scopes: 'read:logs read:users'
   }));
+
   app.use('/meta', meta());
   app.use('/.extensions', hooks());
 
   app.use('/app', Express.static(path.join(__dirname, '../dist')));
+
+  app.use(processLogs(storage));
   app.use('/', routes(storage));
 
   // Generic error handler.
